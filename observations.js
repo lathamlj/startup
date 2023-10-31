57cf87203063c2f5c89e8867a22fdd465d6c129c
Latham Jeppson <lljeppson7@gmail.com>
2023-10-30 19:47:27 -0600

    added js and JSON for observation functionality

--- observations.js
+++ observations.js
@@ -1,28 +1,64 @@
-// class Observations {
-
-//     constructor() {
-//         const playerNameEl = document.querySelector('.player-name');
-//         playerNameEl.textContent = this.getPlayerName();
-//     }
-
-//     getPlayerName() {
-//         return localStorage.getItem('userName') ?? 'Mystery player';
-//       }
-// }
-
 class Observations {
     constructor() {
-        // Wait for the DOM to fully load
+        //wait for the DOM to fully load
         document.addEventListener('DOMContentLoaded', () => {
             const userNameEl = document.querySelector('.userName');
             userNameEl.textContent = this.getUserName();
+
+            const observationTextArea = document.querySelector('#floatingTextarea2');
+            const saveButton = document.querySelector('#save-observation');
+            const observationsContainer = document.querySelector('#observations-container');
+
+            //load the user's previous observation
+            observationTextArea.value = this.getObservation();
+
+            //sdd a click event listener to the "Save" button
+            saveButton.addEventListener('click', () => {
+                const observation = observationTextArea.value;
+                this.saveObservation(observation);
+
+                //display the user's observation and name
+                const userName = this.getUserName();
+                this.displayObservation(userName, observation, observationsContainer);
+            });
         });
     }
 
     getUserName() {
-        return localStorage.getItem('userName') ?? 'Mystery user';
+        return localStorage.getItem('userName') || 'Mystery user';
+    }
+
+    getObservation() {
+        return localStorage.getItem('userObservation') || '';
+    }
+
+    saveObservation(observation) {
+        localStorage.setItem('userObservation', observation);
+    }
+
+    displayObservation(userName, observation, container) {
+        const observationData = { userName, observation };
+        this.saveObservationToJSON(observationData);
+        this.updateObservationsUI(container);
+    }
+
+    saveObservationToJSON(observationData) {
+        let observations = JSON.parse(localStorage.getItem('observations')) || [];
+        observations.push(observationData);
+        localStorage.setItem('observations', JSON.stringify(observations));
+    }
+
+    updateObservationsUI(container) {
+        const observations = JSON.parse(localStorage.getItem('observations')) || [];
+
+        let observationHTML = '';
+        for (const data of observations) {
+            observationHTML += `<p>${data.userName} made an observation: ${data.observation}</p>`;
+        }
+
+        container.innerHTML = observationHTML;
     }
 }
 
-// Instantiate the Observations class when the script loads
+//instantiate the Observations class when the script loads
 new Observations();
