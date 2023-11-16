f0963c45d73f1b3e66a43f0dd709cab45d78dbe8
Latham Jeppson <lljeppson7@gmail.com>
2023-11-16 15:40:51 -0700

    thirdparty service endpoint added

--- index.js
+++ index.js
@@ -16,30 +16,16 @@ app.use(`/api`, apiRouter);
 
 // Get Observations
 apiRouter.get('/observations', (_req, res) => {
-  // Implementation to retrieve and send observations
-  // For now, let's send a placeholder response
-  res.json({ observations: [] });
+  res.send(observations);
+  //res.json({ observations: [] });
 });
 
 // Save Observation
 apiRouter.post('/observations', (req, res) => {
-  // Implementation to save a new observation
-  // For now, let's send a placeholder response
-  res.json({ message: 'Observation saved successfully!' });
-});
-
-// Get User Name
-apiRouter.get('/username', (_req, res) => {
-  // Implementation to retrieve and send the user's name
-  // For now, let's send a placeholder response
-  res.json({ username: 'Mystery user' });
-});
-
-// Save User Name
-apiRouter.post('/username', (req, res) => {
-  // Implementation to save or update the user's name
-  // For now, let's send a placeholder response
-  res.json({ message: 'Username saved successfully!' });
+  // save observation in database?
+  observations = saveObservation(req.body, observations);
+  res.send(observations)
+  //res.json({ message: 'Observation saved successfully!' });
 });
 
 // Return the application's default page if the path is unknown
@@ -50,3 +36,9 @@ app.use((_req, res) => {
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+let observations = [];
+function saveObservation(newObservation, observations) {
+    observations.push({ observation: newObservation});
+    return observations;
+}
