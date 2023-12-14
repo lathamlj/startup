ced61b11ecd601abfb2403fbef404f982e2fd296
Latham Jeppson <lljeppson7@gmail.com>
2023-12-13 23:49:04 -0700

    debugging

--- peerproxy.js
+++ peerproxy.js
@@ -18,6 +18,7 @@ function peerProxy(httpServer) {
   wss.on('connection', (ws) => {
     const connection = { id: uuid.v4(), alive: true, ws: ws };
     connections.push(connection);
+    console.log("in 'on' func");
 
     // Forward messages to everyone except the sender
     ws.on('message', function message(data) {
